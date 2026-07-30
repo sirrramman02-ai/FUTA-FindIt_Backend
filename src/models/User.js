ebfb916be6250524aca892c414aa103411da01d5
Yoramman <sirrramman02@gmail.com>
2026-07-30 23:13:25 +0100

    Button2

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,3 @@
 import mongoose from 'mongoose'
-const schema = new mongoose.Schema({ name: String, email: { type: String, unique: true }, passwordHash: String, studentId: String, department: String, faculty: String, level: String, role: { type: String, default: 'student' }, suspended: { type: Boolean, default: false } }, { timestamps: true })
+const schema = new mongoose.Schema({ username: { type: String, required: true, unique: true }, name: String, email: { type: String, unique: true }, contactEmail: String, contactWhatsapp: String, passwordHash: String, studentId: String, department: String, faculty: String, level: String, role: { type: String, default: 'student' }, suspended: { type: Boolean, default: false } }, { timestamps: true })
 export default mongoose.models.User || mongoose.model('User', schema)
